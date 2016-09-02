bddd8583daf0b0747d5cc765b9f4ba8b77c35e3f
nicol655@gmail.com <jhonnypeca@hotmail.com>
2016-09-02 02:21:56 -0400

    Created Home Template

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to Python Flask!'
+    return render_template('home.html')
 
 #TEST
 @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
@@ -127,6 +127,7 @@ if __name__ == '__main__':
 #    app.debug = True
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    #app.run(host='127.0.0.1', port=port)
 
 
 #ERROR HANDLERS
