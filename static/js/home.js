00f3c6956de4b559080bb5687b4fcba7bd359f58
Daniela Roa <danielaroa1407@gmail.com>
2016-09-15 00:45:39 -0400

    Created Proposal Modal for steps 1 and 2

--- static/js/home.js
+++ static/js/home.js
@@ -25,4 +25,25 @@ $(document).ready( function() {
 
   var volunter_percent = $('#volunters--percent').val();
   $('.newsfeed--bar2').css('width',volunter_percent+'%');
+
+  $('.next--proposal1').on('click',function(){
+    $('#modal_proposal1').css('visibility','hidden');
+  });
+  /*** check all proposal step 2 ***/
+    $("#select_all").change(function(){  //"select all" change 
+    $(".checkbox").prop('checked', $(this).prop("checked")); //change all ".checkbox" checked status
+});
+
+//".checkbox" change 
+$('.checkbox').change(function(){ 
+    //uncheck "select all", if one of the listed checkbox item is unchecked
+    if(false == $(this).prop("checked")){ //if this item is unchecked
+        $("#select_all").prop('checked', false); //change "select all" checked status to false
+    }
+    //check "select all" if all checkbox items are checked
+    if ($('.checkbox:checked').length == $('.checkbox').length ){
+        $("#select_all").prop('checked', true);
+    }
+});
+  /** end of check all  ***/
 });
