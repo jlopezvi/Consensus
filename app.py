1c54865bba9503e8de521a475439aad504230825
Daniela Roa <nicol655@gmail.com>
2016-09-24 00:18:22 +0200

    Fixed bugs

--- app.py
+++ app.py
@@ -306,5 +306,5 @@ def getConcerns(current):
 if __name__ == '__main__':
     #app.debug = True
     port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
+    #app.run(host='127.0.0.1', port=port)
