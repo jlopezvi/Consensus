5cf139520f3d9c36e9e84aada7bcca2951326d00
Nicol Ridente <danielaroa1407@gmail.com>
2016-10-18 17:16:23 -0400

    changing database url for heroku

--- utils.py
+++ utils.py
@@ -3,4 +3,4 @@ from py2neo import neo4j
 def getGraph() :
      #print(neo4j, neo4j.__file__)
      #return neo4j.GraphDatabaseService("http://localhost:7474/db/data")
-     return neo4j.GraphDatabaseService("http://hobby-kjjhomhijildgbkehfaomgol.dbs.graphenedb.com:24789/db/data")
+     return neo4j.GraphDatabaseService("http://app55594714-V1ivYS:QZgiH1f3jWNWOC2yUYZK@hobby-kjjhomhijildgbkehfaomgol.dbs.graphenedb.com:24789")
