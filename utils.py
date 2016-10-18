0e54c7c0a91e671540498dab290060d1d3ad5e19
Nicol Ridente <danielaroa1407@gmail.com>
2016-10-18 18:36:07 -0400

    changing database url for heroku

--- utils.py
+++ utils.py
@@ -9,20 +9,20 @@ except ImportError:
 def getGraph():
      #print(neo4j, neo4j.__file__)
      #return neo4j.GraphDatabaseService("http://localhost:7474/db/data")
-     #return neo4j.GraphDatabaseService("http://app55594714-V1ivYS:QZgiH1f3jWNWOC2yUYZK@hobby-kjjhomhijildgbkehfaomgol.dbs.graphenedb.com:24789")
-
+     return neo4j.GraphDatabaseService("http://app55594714-V1ivYS:QZgiH1f3jWNWOC2yUYZK@hobby-kjjhomhijildgbkehfaomgol.dbs.graphenedb.com:24789")
+'''
      if os.environ.get('GRAPHENEDB_URL'):
           try:
                graph_db_url = urlparse(os.environ.get('GRAPHENEDB_URL'))
           except ImportError:
                graph_db_url = urljoin(os.environ.get('GRAPHENEDB_URL'))
           graph_db = neo4j.GraphDatabaseService(
-               'GRAPHENEDB_URL')
-               #     .format(
-               #     host=graph_db_url.hostname,
-               #     port=graph_db_url.port,
-               #     path=graph_db_url.path
-               #))
+               'http://{host}:{port}{path}'.format(
+                    host=graph_db_url.hostname,
+                    port=graph_db_url.port,
+                    path=graph_db_url.path
+               ), user_name=graph_db_url.username, password=graph_db_url.password)
      else:
           graph_db = neo4j.GraphDatabaseService('http://localhost:7474/db/data')
      return graph_db
+'''
