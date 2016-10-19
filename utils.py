d7098eb6a602673faec374b34a44a1a93f01d792
Nicol Ridente <danielaroa1407@gmail.com>
2016-10-19 12:54:16 -0400

    changing config variables

--- utils.py
+++ utils.py
@@ -1,9 +1,6 @@
 from py2neo import neo4j
 import os
-try:
-     from urlparse import urlparse
-except ImportError:
-     from urllib.parse import urljoin
+from urllib.parse import urlparse
 
 
 def getGraph():
@@ -12,12 +9,10 @@ def getGraph():
      #return neo4j.GraphDatabaseService("http://app55594714-V1ivYS:QZgiH1f3jWNWOC2yUYZK@hobby-kjjhomhijildgbkehfaomgol.dbs.graphenedb.com:24789")
 
      if os.environ.get('GRAPHENEDB_URL'):
-          try:
-               graph_db_url = urlparse(os.environ.get('GRAPHENEDB_URL'))
-          except ImportError:
-               graph_db_url = urljoin(os.environ.get('GRAPHENEDB_URL'))
-          graph_db = neo4j.GraphDatabaseService(graph_db_url)
+         graph_db_url = urlparse(os.environ.get('GRAPHENEDB_URL'))
+         neo4j.authenticate("{host}:{port}".format(host=graph_db_url.hostname, port=graph_db_url.port), graph_db_url.username, graph_db_url.password)
+
+         graph_db = neo4j.GraphDatabaseService('http://{host}:{port}/db/data'.format(host=graph_db_url.hostname, port=graph_db_url.port))
      else:
-          graph_db = neo4j.GraphDatabaseService('http://localhost:7474/db/data')
-          print(graph_db)
+         graph_db = neo4j.GraphDatabaseService('http://localhost:7474/db/data')
      return graph_db
