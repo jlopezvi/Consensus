d6c511274f582e607306db3003854ecddc5b92e7
Nicol Ridente <danielaroa1407@gmail.com>
2016-10-24 16:23:40 -0400

    set email validated = false, to normal

--- static/js/login.js
+++ static/js/login.js
@@ -133,7 +133,7 @@ $(document).ready( function() {
       'image_url': '', //$('#password').val(),
       'ifpublicprofile': opt,
       'host_email': null,
-      'ifemailverified': true
+      'ifemailverified': false
     };
     $.ajax({
       url: 'registration',
