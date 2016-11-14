832b030085f59ab357b9201ced90ae9061a83239
Nicol Ridente <danielaroa1407@gmail.com>
2016-11-14 12:03:53 -0400

    added some changes to template

--- static/js/newsfeed.js
+++ static/js/newsfeed.js
@@ -1,3 +1,6 @@
+var url = window.location.href;
+url = url.split("/");
+
 $(document).ready(function(){
 	$('.home--content').css('background-image','url("")');
 	var percent = $('#percent').val();
@@ -8,4 +11,28 @@ $(document).ready(function(){
 
 	var volunter_percent = $('#volunters--percent').val();
 	$('.newsfeed--bar2').css('width',volunter_percent+'%');
+
+	$.ajax({
+      url: url[0] + "//" + url[2] + '/ideas_for_newsfeed',
+      type: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      dataType: 'json',
+      success: function(json) {
+        console.log(json);
+      },
+      error: function(response){
+        console.log('error');
+        console.log(response);
+        /*
+        if(response.responseText == "redirect to Home"){
+          alert("Redirecting to Home");
+          //window.location = '/home';
+        } else {
+
+        }
+        */
+      }
+    });
 });
