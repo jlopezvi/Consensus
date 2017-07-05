676a690f4a2a798ba28c3254890d052da99566da
Nicol <nicol655@gmail.com>
2017-07-05 14:55:40 +0000

    Added functions to share buttons at HOME. Added functions to progress bar at HOME.

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,7 @@
+var url = window.location.href;
+url = url.split("/");
+var list;
+
 $(document).ready( function() {
   $('.home--select div').slideUp();
 	$('.select--triangle').on('click',function(){
@@ -18,24 +22,63 @@ $(document).ready( function() {
     $('.select--options').slideUp(100);
     $('.select--triangle').removeClass('active-triangle');
     
-    $('#home--proposals').empty();
+    $('#home--proposals').empty().hide();
     loadHomeIdeas(type_vote);
   });
   
-  var percent = $('#percent').val();
-  var total = Math.floor(percent);
-  $('.input--percent').append(total + '%');
-  var support_percent = $('#supporters--percent').val();
-  $('.newsfeed--bar').css('width',support_percent+'%');
-
-  var volunter_percent = $('#volunters--percent').val();
-  $('.newsfeed--bar2').css('width',volunter_percent+'%');
-  
   loadHomeIdeas('supported');
+  
+  $(document).on('click', '.home--share--button', function(){
+    var vote_type = $(this).attr('id');
+    var index_idea = $(this).parent().children('input').val();
+    var vote_ifvolunteered = false;
+    if(vote_type == 'supported-plus'){
+      vote_ifvolunteered = true;
+      vote_type = 'supported';
+    }
+    var element = $(this).parent().parent().parent();
+      
+    var data = {
+      'idea_proposal': list[index_idea]['proposal'],
+      'vote_ifvolunteered': vote_ifvolunteered,
+      'vote_type': vote_type
+    };
+      
+    $.ajax({
+      url: url[0] + "//" + url[2] + '/vote_on_idea',
+      type: 'POST',
+      data: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      dataType: 'json',
+      success: function(json){
+        //console.log(json);
+        if(data.vote_type == 'supported' || data.vote_type == 'supported-plus')
+          var answer = 'Now you are supporting this idea!';
+        else
+          var answer = 'Idea rejected successfully!';
+        if(json.result == 'OK: User vote was modified'){
+          $('#invitation-modal-info h4.modal-title').empty().append('Operation Completed');
+          $('#invitation-modal-info p#modal--invitation').empty().append(answer);
+          element.fadeOut('slow', function(){ element.remove(); });
+        } else if (json.result == 'Wrong: User vote exists of same type'){
+          $('#invitation-modal-info h4.modal-title').empty().append('Error');
+          $('#invitation-modal-info p#modal--invitation').empty().append('You can not vote the same for this idea.');
+        }
+        $('#invitation-modal-info').modal('toggle');
+      },
+      error: function(response){
+        console.log('error');
+        console.log(response);
+      }
+  	});
+  });
 
 }); 
 
 function loadHomeIdeas(type_vote){
+  $('.spinner').show();
   $.ajax({
     url: url[0] + "//" + url[2] + '/ideas_for_home',
     type: 'POST',
@@ -44,10 +87,15 @@ function loadHomeIdeas(type_vote){
       'Content-Type': 'application/json'
     },
     success: function(json){
-      console.log(json.data);
+      //console.log(json.data);
+      list = json.data;
       for(i=0; i<json.data.length;i++){
-        showIdeas(json.data[i]);
+        showIdeas(json.data[i], i);
       }
+      setTimeout(function(){
+        $('#home--proposals').show();
+        $('.spinner').hide();
+      }, 2500);
     },
     error: function(response){
       console.log('error');
@@ -56,7 +104,7 @@ function loadHomeIdeas(type_vote){
 	});
 }
 
-function showIdeas(list){
+function showIdeas(list, index){
   var code = '';
   code += '<div class="col-sm-6 home--content--proposal">';
   code += '<div class="row home--header">';
@@ -67,22 +115,23 @@ function showIdeas(list){
 	code += '<p><img style="width: 19px;position: relative;top: -3px;" src="/static/images/clock-icon.png">&nbsp;';
 	code += list.duration+'</p></div>';
 	code += '<div class="col-sm-3 home--charge"><div class="progress home--progress">';
-	code += '<input type="text" value="'+(list.supporters_num*100)/list.supporters_goal_num+'" id="supporters--percent" hidden>';
-	code += '<div class="progress-bar newsfeed--bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style=""></div>';
+	var support_percent = (list.supporters_num*100)/list.supporters_goal_num;
+	code += '<div class="progress-bar newsfeed--bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:'+support_percent+'%"></div>';
 	code += '</div>	<div class="progress home--progress2">';
-	code += '<input type="text" value="'+(list.volunteers_num*100)/list.volunteers_goal_num+'" id="volunters--percent" hidden>';
-	code += '<div class="progress-bar newsfeed--bar2" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style=""></div>';
+	var volunter_percent = (list.volunteers_num*100)/list.volunteers_goal_num;
+	code += '<div class="progress-bar newsfeed--bar2" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:'+volunter_percent+'%"></div>';
 	code += '</div></div><div class="col-sm-4 newsfeed--goals">';
 	code += '<p>'+list.supporters_num+'/'+list.supporters_goal_num+' supporters goals<br>';
 	code += '<p>'+list.volunteers_num+'/'+list.volunteers_goal_num+' volunteers goals<br>';
-	code += '</div></div><div class="row home--proposals--body"><div class="col-sm-12">';
-	code += '	<div class="col-sm-8 newsfeed--problem">'+list.concern+'</div>';
+	code += '</div></div><div class="row home--proposals--body" style="background-image: url('+list.image_url+');">';
+	code += '<div class="col-sm-12"><div class="col-sm-8 newsfeed--problem">'+list.concern+'</div>';
 	code += '</div><div class="col-sm-12" style="margin-top: 180px;"><div class="col-sm-8 col-sm-offset-4 newsfeed--proposal">'+list.proposal+'</div></div>';
 	code += '</div><div class="row newsfeed--footer">';
 	code += '<div class="col-sm-12" style="padding-right: 0px; padding-left: 0px;"><div class="col-sm-1 redflag--img">';
 	code += '<img src="/static/images/redflag.png"></div><div class="col-sm-9 newsfeed--support" style="padding-right:0;padding-left:30px;">';
-	code += '<input type="text" value="" id="percent" hidden>';
-	code += '<div class="input--percent"><label>	Support Rate: </label></div>';
+  var percent = (list.supporters.length * 100)/(list.supporters.length + list.rejectors.length);
+  var total = Math.floor(percent);
+	code += '<div class="input--percent"><label>	Support Rate: '+total+'%</label></div>';
 	code += '</div>	<div class="col-sm-2 neewsfeed--moreinfo" style="float:right;"><input type="button" name="more-info" class="home--button">';
 	code += '</div></div></div><div class="row newsfeed--persons"><div class="col-sm-12">';
 	code += '<div class="col-sm-1" style="padding:0;"><img src="/static/images/check-small.png"></div>';
@@ -117,9 +166,12 @@ function showIdeas(list){
   else if(list.rejectors.length == 0){
     code += '<a href="#" class="last--liked"><li>0 people</li></a>';
   }
-  code += '</ul></div></div></div><div class="row home--share"><div class="col-sm-12 home--share--icons">';
-	code += '<div class="col-sm-6" style="padding:0;"><img src="/static/images/x-icon.png">';
-	code += '<img style="width: 50px;" src="/static/images/check-icon.png"><img style="width: 48px;" src="/static/images/checkmark.png">';
+  code += '</ul></div></div></div><div class="row home--share"><div class="col-sm-6 home--share--icons">';
+  code += '<input type="hidden" class="index--idea" value="'+index+'"';
+	code += '<div class="col-sm-6" style="padding:0;">';
+	code += '<img src="/static/images/x-icon.png" class="home--share--button" id="rejected">';
+	code += '<img style="width: 50px;" src="/static/images/check-icon.png" class="home--share--button" id="supported">';
+	code += '<img style="width: 48px;" src="/static/images/checkmark.png" class="home--share--button" id="supported-plus">';
 	code += '</div><div class="col-sm-6 home--followers"><i class="fa fa-share-alt"></i><p>Share with: followers</p></div></div></div></div>';
 	
 	$('#home--proposals').append(code);
