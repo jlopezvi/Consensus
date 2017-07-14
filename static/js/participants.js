dd809bf4626cf3702bbfb467e3a1250146a86708
Nicol <nicol655@gmail.com>
2017-07-14 18:16:38 +0000

    Added validation to emails at send_invitation

--- static/js/participants.js
+++ static/js/participants.js
@@ -196,23 +196,39 @@ $(document).ready( function() {
 	$('body').removeClass('container').addClass('container-fluid');
 	
 	$(document).on('click', '#send_invitation-btn', function(){
-		$('#send_invitation-btn').prop('disabled', true);
 		var guest_email = $('#emails_input').val().split(",");
 		var host_email = $('#host_email').val();
 		var msg = '';
-		for(var i=0; i<guest_email.length; i++){
-			$.ajax({
-				url: url[0] + "//" + url[2] + '/registration_send_invitation/'+host_email+'/'+guest_email[i],
-				type: 'GET'
-			});
-			msg += '<br><strong>'+guest_email[i]+'</strong>';
+		var validation = false;
+		if(guest_email != ''){
+			for(var i=0; i<guest_email.length; i++){
+				if( !isValidEmailAddress( guest_email[i] ) ) {
+					validation = true;
+					break;
+				}
+			}
+		} else
+			validation = true;
+		if(validation)
+			$('#emails_input').css('border-color', '#FF0000').after('<span class="span--error"><br>All emails must be valid!</span>');
+		else{
+			$('#send_invitation-btn').prop('disabled', true);
+			$('#emails_input').css('border-color', 'rgb(169, 169, 169)');
+			$('.span--error').remove();
+			for(var i=0; i<guest_email.length; i++){
+				$.ajax({
+					url: url[0] + "//" + url[2] + '/registration_send_invitation/'+host_email+'/'+guest_email[i],
+					type: 'GET'
+				});
+				msg += '<br><strong>'+guest_email[i]+'</strong>';
+			}
+			setTimeout(function(){
+				$('#send_invitation-btn').prop('disabled', false);
+				$('.modal-title').empty().append('Success!!');
+				$('.modal-body p#modal--invitation').empty().append('An email with an invitation sent to:'+msg);
+				$('#invitation-modal-info').modal('toggle');
+			},3000);
 		}
-		setTimeout(function(){
-			$('#send_invitation-btn').prop('disabled', false);
-			$('.modal-title').empty().append('Success!!');
-			$('.modal-body p#modal--invitation').empty().append('An email with an invitation sent to:'+msg);
-			$('#invitation-modal-info').modal('toggle');
-		},3000);
 	});
 
 	$('#search-input-participant').on('keydown', function(e){
@@ -600,6 +616,11 @@ $(document).ready(function(){
     });  
 
 
-});   
+});
+
+function isValidEmailAddress(emailAddress) {
+	var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+	return pattern.test(emailAddress);
+};
 
 
