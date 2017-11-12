ac09b0ea464651d47cb1437c52fee3145bf41d5e
jlopezvi <jjlopezvillarejo@gmail.com>
2017-11-12 20:08:24 +0100

    port change to 80

--- app.py
+++ app.py
@@ -948,6 +948,6 @@ def getAllCommunitiesForUser(email):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     if os.environ.get('GRAPHENEDB_URL'):
-        app.run(host='0.0.0.0', port=port)
+        app.run(host='0.0.0.0', port=80)
     else:
         app.run(host='127.0.0.1', port=port)
